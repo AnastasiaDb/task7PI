23684de3c90931dcf5bb79fefe22a5fcaa60ae2f
Anastasia <82052788+AnastasiaDb@users.noreply.github.com>
2023-12-13 22:42:43 +0300

    Добавьте файлы проекта.

---
+++ task7PI/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите значение для a:");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите значение для b:");
+        int b = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите значение для c:");
+        int c = int.Parse(Console.ReadLine());
+
+        if (IsPythagoreanTriple(a, b, c))
+        {
+            Console.WriteLine($"Тройка ({a}, {b}, {c}) является тройкой Пифагора.");
+        }
+        else
+        {
+            Console.WriteLine($"Тройка ({a}, {b}, {c}) не является тройкой Пифагора.");
+        }
+    }
+
+    static bool IsPythagoreanTriple(int a, int b, int c)
+    {
+        return a * a + b * b == c * c;
+    }
+}
