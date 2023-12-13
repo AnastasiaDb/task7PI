b2485043128199b6b29de85842936bb9f6a2d263
Anastasia <82052788+AnastasiaDb@users.noreply.github.com>
2023-12-13 23:01:21 +0300

    add mistake

--- task7PI/Program.cs
+++ task7PI/Program.cs
@@ -10,7 +10,7 @@ class Program
         Console.WriteLine("Введите значение для b:");
         int b = int.Parse(Console.ReadLine());
 
-        Console.WriteLine("Введите значение для c:");
+        Console.WriteLine("Введите значение для c:")
         int c = int.Parse(Console.ReadLine());
 
         if (IsPythagoreanTriple(a, b, c))
