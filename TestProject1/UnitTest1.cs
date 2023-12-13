6b908554af2d69e4f926fab7c7a15e9ad0d40cca
Anastasia <82052788+AnastasiaDb@users.noreply.github.com>
2023-12-14 00:19:17 +0300

    add test

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -1,4 +1,4 @@
-
+using 
 
 namespace TestProject1
 {
@@ -6,8 +6,46 @@ namespace TestProject1
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void IsPythagoreanTriple_ShouldReturnTrue_ForPythagoreanTriple()
         {
+            var Program = new Program();
+
+            int a = 3;
+            int b = 4;
+            int c = 5;
+
+            bool result = Program.IsPythagoreanTriple(a, b, c);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void IsPythagoreanTriple_ShouldReturnFalse_ForNonPythagoreanTriple()
+        {
+            var Program = new Program();
+            int a = 2;
+            int b = 3;
+            int c = 6;
+
+            bool result = Program.IsPythagoreanTriple(a, b, c);
+
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void IsPythagoreanTriple_ShouldHandleZeros()
+        {
+            var Program = new Program();
+
+            int a = 0;
+            int b = 0;
+            int c = 0;
+
+            bool result = Program.IsPythagoreanTriple(a, b, c);
+
+
+            Assert.IsTrue(result); // 0^2 + 0^2 = 0^2
         }
     }
 }
